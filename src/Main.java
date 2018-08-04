d5125b3cec459280775f392b39206a563e4a3be5
Desai <srdesai@lm-maa-22004945.paypalcorp.com>
2018-08-04 12:30:43 +0530

    bfs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import javafx.util.Pair;
+
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -30,10 +32,53 @@ public class Main {
 
     }*/
 
+    public static boolean isValidWord(String query) {
+        return Collections.binarySearch(extractedDict, query) > 0 ;
+    }
+
+    public static ArrayList<String> getAllPossibleWords(String s) {
+        ArrayList<String> allWordsPossible = new ArrayList<>();
+        for(int i=0;i<s.length();i++) {
+            for(int j=0;j<((int)'Z' - (int)'A');j++) {
+                if(s.charAt(i) != (char)(j + (int)'A')) {
+                    StringBuilder newWord = new StringBuilder(s);
+                    newWord.setCharAt(i, (char)(j + (int)'A'));
+                    if(isValidWord(newWord.toString())) {
+                        allWordsPossible.add(newWord.toString());
+                    }
+                }
+            }
+        }
+        return allWordsPossible;
+    }
+
+    public static void bfs(String startingWord, String endWord) {
+        Queue<Pair<String, Integer>> que = new LinkedList<>();
+        que.add(new Pair<>(startingWord, 0));
+        HashMap<String, Integer> map = new HashMap<>();
+        while(!que.isEmpty()) {
+            Pair<String, Integer> curWord = que.peek();
+            que.remove();
+            if(map.get(curWord.getKey()) != null) {
+                continue;
+            }
+            map.put(curWord.getKey(), 1);
+            if(curWord.getKey().equals(endWord)) {
+                System.out.println(curWord.getValue());
+                break;
+            }
+            ArrayList<String> allPossibleWords = getAllPossibleWords(curWord.getKey());
+            for(String s: allPossibleWords) {
+                que.add(new Pair<>(s, curWord.getValue() + 1));
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         System.out.println("asdf");
         loadDictionary("CLAY");
+        bfs("CLAY", "GOLD");
     }
 
 
